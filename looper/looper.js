75a093e629d1eaea914e3f6784f0047b778ef8bd
busticated <itsbusticated@gmail.com>
2011-10-29 19:33:47 -0700

    check for function type before executing, rate method restarts loop so that rate change is immediate

--- looper/looper.js
+++ looper/looper.js
@@ -2,14 +2,45 @@ define( function(){
     var funcs = [], //cache of functions to run each iteration
         loopDuration = 1000, //in ms
         isRunning = false,
-        start = function( func, spd ){ add( func ); isRunning = true; loopDuration = spd || loopDuration; setTimeout( function(){ _loop(); }, loopDuration ); },
-        stop = function(){ isRunning = false; },
-        rate = function( spd ){ if ( typeof spd === 'undefined' ){ return loopDuration; } loopDuration = spd; },
+        timerId = null,
+
+        //starts loop, adds funcion to funcs cache (opt) & set loopDuration (opt)
+        start = function( func, spd ){
+            if ( typeof func === 'function' ) {
+                add( func );
+            }
+
+            if ( isRunning ) {
+                return;
+            }
+
+            isRunning = true;
+            loopDuration = spd || loopDuration;
+            setTimeout(
+                function(){
+                    _loop();
+            }, loopDuration );
+        },
+
+        //stops loop immediately
+        stop = function(){ isRunning = false; clearTimeout( timerId ); },
+
+        //gets & sets loopDuration, restarts loop after setting loopDuration
+        rate = function( spd ){
+            if ( typeof spd === 'undefined' ){
+                return loopDuration;
+            }
+
+            loopDuration = spd;
+            stop();
+            start();
+        },
+
         add = function( func ) { funcs.push( func ); },
         clear = function() { funcs = []; },
         _loop = function(){
             if ( ! isRunning ){
-                return false;
+                return;
             }
 
             for ( var i = 0, length = funcs.length; i < length; i = i + 1 ) {
@@ -18,7 +49,7 @@ define( function(){
                 }
             }
 
-            setTimeout( _loop, loopDuration );
+            timerId = setTimeout( _loop, loopDuration );
         };
 
     // public api /////////////////////////////////////////////////////////////
