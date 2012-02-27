5c66a658997dccad769fddeb2acb851ef7c67195
busticated <itsbusticated@gmail.com>
2012-02-26 18:33:07 -0800

    make eventer a proper factory, update tests

--- eventer/eventer.js
+++ eventer/eventer.js
@@ -1,14 +1,21 @@
 define( function(){
-    //main event hash
-    var _eventCache = {},
-
-    //add an event and callback
-    on = function( eventName, callback ){
-        if ( ! _eventCache[ eventName ] ) {
-            _eventCache[ eventName ] = [];
+    var Eventer = function(){
+        if ( ! ( this instanceof Eventer ) ){
+            return new Eventer();
         }
 
-        _eventCache[ eventName ].push( callback );
+        //main event hash
+        this._eventCache = {};
+    };
+
+    //add an event and callback
+    Eventer.prototype.on = function( eventName, callback ){
+        var self = this;
+        if ( ! this._eventCache[ eventName ] ) {
+            this._eventCache[ eventName ] = [];
+        }
+
+        this._eventCache[ eventName ].push( callback );
 
         //simple version - just return an event object
         //return [ eventName, callback ];
@@ -19,45 +26,41 @@ define( function(){
         return {
             _handle : [ eventName, callback ],
             off : function(){
-                off( [ eventName, callback ] );
+                self.off( [ eventName, callback ] );
             },
             do : function( data ){
                 callback( data );
             }
         }
-    },
+    };
 
     //remove callback and event if appropriate
     //todo: remove all handlers if argument is a string?
-    off = function( eventHandle ){
+    Eventer.prototype.off = function( eventHandle ){
         var eventName = eventHandle[ 0 ];
 
-        if ( ! _eventCache[ eventName ] ){
+        if ( ! this._eventCache[ eventName ] ){
             return;
         }
 
-        for ( var i = 0; i < _eventCache[ eventName ].length; i = i + 1 ) {
-            if ( _eventCache[ eventName ][ i ] === eventHandle[ 1 ] ){
-                _eventCache[ eventName ].splice( i, 1 );
+        for ( var i = 0; i < this._eventCache[ eventName ].length; i = i + 1 ) {
+            if ( this._eventCache[ eventName ][ i ] === eventHandle[ 1 ] ){
+                this._eventCache[ eventName ].splice( i, 1 );
             }
         }
-    },
+    };
 
     //fire event callbacks
-    emit = function( eventName, data ){
-        if ( ! _eventCache[ eventName ] ){
+    Eventer.prototype.emit = function( eventName, data ){
+        if ( ! this._eventCache[ eventName ] ){
             return;
         }
 
-        for ( var i = 0; i < _eventCache[ eventName ].length; i = i + 1 ) {
-            _eventCache[ eventName ][ i ].call( this, data );
+        for ( var i = 0; i < this._eventCache[ eventName ].length; i = i + 1 ) {
+            this._eventCache[ eventName ][ i ].call( this, data );
         }
     };
 
     // public api /////////////////////////////////////////////////////////////
-    return {
-        on: on,
-        off: off,
-        emit: emit
-    }
+    return Eventer;
 });
