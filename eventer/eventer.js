79d95dabdea227a9c7ae38198b5748498c4f8bb2
busticated <itsbusticated@gmail.com>
2012-05-26 21:57:38 -0500

    use a common event cache object

--- eventer/eventer.js
+++ eventer/eventer.js
@@ -3,29 +3,28 @@
 define( function(){
     'use strict';
 
+    var eventCache = {};
+
     var Eventer = function(){
         if ( ! ( this instanceof Eventer ) ){
             return new Eventer();
         }
-
-        //main event hash
-        this._eventCache = {};
     };
 
     //add an event and callback
     Eventer.prototype.on = function( eventName, callback ){
         var self = this;
-        if ( ! this._eventCache[ eventName ] ) {
-            this._eventCache[ eventName ] = [];
+        if ( ! eventCache[ eventName ] ) {
+            eventCache[ eventName ] = [];
         }
 
-        this._eventCache[ eventName ].push( callback );
+        eventCache[ eventName ].push( callback );
 
         //simple version - just return an event object
         //return [ eventName, callback ];
 
         //fancy version wrap 'off' in closure to enable
-        //easier access, create 'do' method to trigger 
+        //easier access, create 'do' method to trigger
         //only this callback
         return {
             _handle : [ eventName, callback ],
@@ -43,26 +42,29 @@ define( function(){
     Eventer.prototype.off = function( eventHandle ){
         var eventName = eventHandle[ 0 ];
 
-        if ( ! this._eventCache[ eventName ] ){
+        if ( ! eventCache[ eventName ] ){
             return;
         }
 
-        for ( var i = 0; i < this._eventCache[ eventName ].length; i = i + 1 ) {
-            if ( this._eventCache[ eventName ][ i ] === eventHandle[ 1 ] ){
-                this._eventCache[ eventName ].splice( i, 1 );
+        for ( var i = 0; i < eventCache[ eventName ].length; i = i + 1 ) {
+            if ( eventCache[ eventName ][ i ] === eventHandle[ 1 ] ){
+                eventCache[ eventName ].splice( i, 1 );
             }
         }
     };
 
-    //fire event callbacks
     Eventer.prototype.emit = function( eventName, data ){
-        if ( ! this._eventCache[ eventName ] ){
+        if ( ! eventCache[ eventName ] ){
             return;
         }
 
-        for ( var i = 0; i < this._eventCache[ eventName ].length; i = i + 1 ) {
-            this._eventCache[ eventName ][ i ].call( this, data );
+        for ( var i = 0; i < eventCache[ eventName ].length; i = i + 1 ) {
+            eventCache[ eventName ][ i ].call( this, data );
         }
+    };
+
+    Eventer.prototype.__getEventCache = function() {
+        return eventCache;
     };
 
     // public api /////////////////////////////////////////////////////////////
