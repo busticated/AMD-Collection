2cd94133d9950b5523a6f8d35164cf81d4e2c128
busticated <itsbusticated@gmail.com>
2012-08-26 14:53:38 -0700

    move modules to src/ dir

---
+++ src/eventer.js
@@ -0,0 +1,52 @@
+﻿/*global define: false, require: false */
+define(function () {
+    'use strict';
+
+    var Eventer = function () {
+        if ( !( this instanceof Eventer ) ) {
+            return new Eventer();
+        }
+
+        this.__handlers = {};
+    };
+
+    Eventer.prototype.on = function ( eventName, callback ) {
+        if ( !this.__handlers[ eventName ] ) {
+            this.__handlers[ eventName ] = [];
+        }
+
+        this.__handlers[ eventName ].push( callback );
+
+        return [ eventName, callback ];
+    };
+
+    Eventer.prototype.off = function ( eventHandle ) {
+        var eventName = eventHandle[ 0 ];
+
+        if ( !this.__handlers[ eventName ] ) {
+            return;
+        }
+
+        for ( var i = 0; i < this.__handlers[ eventName ].length; i = i + 1 ) {
+            if ( this.__handlers[ eventName ][ i ] === eventHandle[ 1 ] ) {
+                this.__handlers[ eventName ].splice( i, 1 );
+            }
+        }
+    };
+
+    Eventer.prototype.emit = function ( eventName, data ) {
+        if ( !this.__handlers[ eventName ] ) {
+            return;
+        }
+
+        for ( var i = 0; i < this.__handlers[ eventName ].length; i = i + 1 ) {
+            this.__handlers[ eventName ][ i ].call( this, data );
+        }
+    };
+
+    Eventer.prototype.getEventHandlers = function () {
+        return this.__handlers;
+    };
+
+    return Eventer;
+});
