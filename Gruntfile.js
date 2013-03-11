474c328c77d3835c18f48a2cb3e24c3e60da1b34
busticated <itsbusticated@gmail.com>
2013-03-11 07:45:51 -0700

    upgrade grunt to v0.4.x style

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function( grunt ) {
+    'use strict';
+
+    // Project configuration.
+    grunt.initConfig({
+        watch: {},
+        qunit: {
+            all: {
+                options: {
+                    urls: [
+                        'http://dev.local.com:3000/tests/index.html'
+                    ]
+                }
+            }
+        },
+        connect: {
+            server: {
+                options: {
+                    port: 3000,
+                    base: '.'
+                }
+            }
+        }
+    });
+
+    // Plugins
+    grunt.loadNpmTasks( 'grunt-contrib-connect' );
+    grunt.loadNpmTasks( 'grunt-contrib-watch' );
+    grunt.loadNpmTasks( 'grunt-contrib-qunit' );
+    grunt.loadNpmTasks( 'grunt-volo' );
+
+    // Tasks
+    grunt.registerTask( 'default', [ 'connect', 'watch' ] );
+    grunt.registerTask( 'test', [ 'connect', 'qunit' ] );
+};
