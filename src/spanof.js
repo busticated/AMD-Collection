3ce1494425fefe2e9d29bfc4097418761cbb2d2b
busticated <itsbusticated@gmail.com>
2013-09-29 15:49:51 -0700

    not sure what I was thinking here...let's just... yeah, that's better

--- src/spanof.js
+++ src/spanof.js
@@ -6,39 +6,33 @@ define(function(){
     var spanOf = function( span ){
 
         var milliseconds = function(){ return span; },
-            ms = milliseconds,
             seconds = function(){ return span * 1000; },
-            sec = seconds,
             minutes = function(){ return this.seconds() * 60; },
-            min = minutes,
             hours = function(){ return this.minutes() * 60; },
-            hr = hours,
             days = function(){ return this.hours() * 24; },
             weeks = function(){ return this.days() * 7; },
-            wk = weeks,
-            years = function(){ return +( this.days() * 365.24219878125 ).toFixed(); },
-            yr = years;
+            years = function(){ return +( this.days() * 365.24219878125 ).toFixed(); };
 
         return {
             milliseconds: milliseconds,
-            ms: ms,
+            ms: milliseconds,
             seconds: seconds,
             second: seconds,
-            sec: sec,
+            sec: seconds,
             minutes: minutes,
             minute: minutes,
-            min: min,
+            min: minutes,
             hours: hours,
             hour: hours,
-            hr: hr,
+            hr: hours,
             day: days,
             days: days,
             weeks: weeks,
             week: weeks,
-            wk: wk,
+            wk: weeks,
             years: years,
             year: years,
-            yr: yr
+            yr: years
         };
     };
 
